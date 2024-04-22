96a3e1f76601c6a4fffaa36be65debfb853c1fd3
Abdalrahman <https://github.com/AB-DALRAHM-AN>
2024-04-22 07:06:07 +0200

    edit blog slug file

--- app/blog/posts/[...slug]/page.tsx
+++ app/blog/posts/[...slug]/page.tsx
@@ -1,48 +1,47 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import { useTheme } from "next-themes"; // Import useTheme from next-themes
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-export async function generateStaticParams() {
-  const postsDir = path.join(process.cwd(), "posts");
+import { MDXRemote } from 'next-mdx-remote/rsc'
+
+export async function generateStaticParams(){
+
+  const postsDir = path.join(process.cwd(), 'posts'); // Get the absolute path to the posts directory
+
   const files = fs.readdirSync(postsDir);
 
-  const paths = files.map((filename) => ({
-    params: {
-      slug: filename.replace(".mdx", "").split("/"),
-    },
+  const paths = files.map(filename => ({
+      params: {
+          slug: filename.replace('.mdx', '').split('/'), // Split the filename into an array of path segments
+      },
   }));
 
   return paths;
 }
 
-function getPost({ slug }: { slug: string }) {
-  const markdownFile = fs.readFileSync(
-    path.join("posts", slug + ".mdx"),
-    "utf-8"
-  );
-  const { data: fontMatter, content } = matter(markdownFile);
+function getPost({slug}: {slug: string}){
 
-  return {
-    fontMatter,
-    slug,
-    content,
-  };
+    const markdownFile = fs.readFileSync(path.join('posts', slug + '.mdx'), 'utf-8')
+
+    const{data: fontMatter, content} = matter(markdownFile)
+
+    return {
+        fontMatter,
+        slug,
+        content
+    }
+
 }
 
-export default function Page({ params }: any) {
-  const props = getPost(params);
-  const { theme } = useTheme(); // Use useTheme hook from next-themes
-  const textColor = theme === "dark" ? "white" : "black"; // Determine text color based on theme
 
-  return (
-    <article
-      className="prose prose-sm md:prose-base lg:prose-lg prose-blue prose-invert my-10 mx-auto"
-      style={{ color: textColor }}
-    >
-      <h1>{props.fontMatter.title}</h1>
-      <MDXRemote source={props.content} />
-    </article>
-  );
+export default function Page({ params } :any){
+    const props = getPost( params);
+
+    return (
+        <article className='prose prose-sm md:prose-base lg:prose-lg prose-blue prose-invert my-10 mx-auto'>
+            <h1>{props.fontMatter.title}</h1>
+
+            <MDXRemote source={props.content} ></MDXRemote>
+        </article>
+    )
 }
