ba19b0905d29136dce2dec9432817e389c905492
Abdalrahman <https://github.com/AB-DALRAHM-AN>
2024-06-14 20:53:06 +0300

    some edit in notion projects api

--- lib/notionProjects.ts
+++ lib/notionProjects.ts
@@ -4,7 +4,7 @@ import {
   PageObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
 
-// Notion Blog API
+// Notion Projects API
 
 // Create a new Notion client
 export const notion = new Client({
@@ -13,36 +13,51 @@ export const notion = new Client({
 
 // Fetch pages from the Notion database
 export const fetchProjects = async () => {
-  const response = await notion.databases.query({
-    database_id: process.env.NEXT_PUBLIC_NOTION_PROJECTS_DATABASE_ID!,
-    filter: {
-      property: "status",
-      status: {
-        equals: "published",
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NEXT_PUBLIC_NOTION_PROJECTS_DATABASE_ID!,
+      filter: {
+        property: "status",
+        status: {
+          equals: "published",
+        },
       },
-    },
-  });
-  return response.results;
+    });
+    return response.results;
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    throw new Error("Failed to fetch projects");
+  }
 };
 
 // Fetch a page by its slug
 export const fetchBySlug = async (slug: string) => {
-  const response = await notion.databases.query({
-    database_id: process.env.NEXT_PUBLIC_NOTION_PROJECTS_DATABASE_ID!,
-    filter: {
-      property: "slug",
-      rich_text: {
-        equals: slug,
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NEXT_PUBLIC_NOTION_PROJECTS_DATABASE_ID!,
+      filter: {
+        property: "slug",
+        rich_text: {
+          equals: slug,
+        },
       },
-    },
-  });
-  return response.results[0] as PageObjectResponse | undefined;
+    });
+    return response.results[0] as PageObjectResponse | undefined;
+  } catch (error) {
+    console.error("Error fetching page by slug:", error);
+    throw new Error("Failed to fetch page by slug");
+  }
 };
 
 // Fetch a page by its ID
 export const fetchProjectsBlocks = async (pageId: string) => {
-  const response = await notion.blocks.children.list({
-    block_id: pageId,
-  });
-  return response.results as BlockObjectResponse[];
+  try {
+    const response = await notion.blocks.children.list({
+      block_id: pageId,
+    });
+    return response.results as BlockObjectResponse[];
+  } catch (error) {
+    console.error("Error fetching page blocks:", error);
+    throw new Error("Failed to fetch page blocks");
+  }
 };
