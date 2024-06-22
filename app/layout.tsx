af9b1d1411c0c1a4450aff4b824453545ea1f906
Abdalrahman <https://github.com/AB-DALRAHM-AN>
2024-06-22 23:21:40 +0300

    opengraph file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Prosto_One } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +17,24 @@ export const metadata: Metadata = {
   },
   description:
     "I'm a self-taught developer with an interest in Computer Science.",
-    twitter: {
+  twitter: {
     card: "summary_large_image",
   },
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://abdalrahman.tech/opengraph-image.png",
-    siteName: "Abdal Rahman",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    title: "Abdal Rahman - Software Engineer",
+    description:
+      "I'm a self-taught developer with an interest in Computer Science.",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "Abdal Rahman",
+      },
+    ],
   },
 };
 
