eb9ca24c1a3055c0e33dca95c1ffb17adc38ee05
Abdelrahman <123552950+AB-DALRAHM-AN@users.noreply.github.com>
2024-06-25 23:42:19 +0300

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -22,11 +22,6 @@ export default function ContactUsForm() {
     reset,
   } = useForm<FormInput>();
 
-  /**
-   * The function sends a POST request to a server with form data and displays a success notification.
-   * @param {FormInput} formData - The formData parameter is an object that contains the input values
-   * from the form.
-   */
   async function onSubmit(formData: FormInput) {
     try {
       const response = await fetch("/api/send", {
@@ -117,96 +112,3 @@ export default function ContactUsForm() {
     </section>
   );
 }
-
-// "use client";
-
-// import emailjs from "@emailjs/browser";
-// import React from "react";
-// import { Input } from "./ui/input";
-// import { Textarea } from "./ui/textarea";
-// import { useState } from "react";
-// import { Button } from "./ui/button";
-// import { useToast } from "@/components/ui/use-toast";
-// import { ToastAction } from "@/components/ui/toast";
-
-// const Contact = () => {
-//   const { toast } = useToast();
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     email: "",
-//     message: "",
-//   });
-
-//   const handleSubmit = (e: any) => {
-//     e.preventDefault();
-//     const { name, email, message } = formData;
-
-//   };
-
-//   const handleInputChange = (e: any) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
-
-//   return (
-//     <section className="flex flex-col justify-center items-center md:gap-8 gap-5 mx-10 my-32 md:mx-40 md:my-24">
-//       <div className="flex justify-center items-center flex-col mb-10 w-full">
-//         <div className="flex flex-col justify-start items-start mb-5">
-//           <h1 className="text-4xl font-bold mb-3">Keep In Touch.</h1>
-//           <p className="text-muted-foreground text-base">
-//             {`I'm`} currently specializing in{" "}
-//             <span className="text-primary">Front-end Development</span>.
-//           </span>
-//           <p className="text-muted-foreground text-base">
-//             Feel free to get in touch and talk more about your projects.
-//           </span>
-//         </div>
-//         <form
-//           className="flex flex-col justify-center items-center gap-4"
-//           onSubmit={handleSubmit}
-//         >
-//           <div className="user-box">
-//             <label>Name</label>
-//             <Input
-//               required
-//               name="name"
-//               placeholder="Your Name"
-//               type="text"
-//               onChange={handleInputChange}
-//               className="md:w-[450px] w-[300px]"
-//             ></Input>
-//           </div>
-//           <div className="user-box">
-//             <label>Email</label>
-//             <Input
-//               placeholder="Your Email"
-//               required
-//               name="email"
-//               type="email"
-//               className="md:w-[450px] w-[300px]"
-//               onChange={handleInputChange}
-//             ></Input>
-//           </div>
-//           <div className="user-box">
-//             <label>Message</label>
-//             <Textarea
-//               required
-//               name="message"
-//               placeholder="Your Message"
-//               onChange={handleInputChange}
-//               className="md:w-[450px] w-[300px]"
-//             ></Textarea>
-//           </div>
-//           <Button variant={"secondary"} className="md:w-[400px] w-[200px]" type="submit">
-//             Send
-//           </Button>
-//         </form>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Contact;
